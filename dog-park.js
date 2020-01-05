7f035bfb7c22777c18d66f82ed13748a71319640
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-01-05 10:43:02 -0700

    Remove class

--- dog-park.js
+++ dog-park.js
@@ -1,20 +1,4 @@
 class DogPark {
-  constructor(name) {
-    this.name = name;
-    this.dogs = [];
-  }
-
-  addDog(dog) {
-    this.dogs.push(dog);
-  }
-
-  removeDog(dog) {
-    for (var i = 0; i < this.dogs.length; i++) {
-      if (this.dogs[i] === dog) {
-        this.dogs.splice(i, 1);
-      }
-    }
-  }
 
 }
 
